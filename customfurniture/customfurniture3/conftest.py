6d1362ca22cd21b8f10cfbef33d6e52ba78e9c1b
Brian Maclin <bmacli6@wgu.edu>
2024-12-09 22:36:53 -0500

    init commit

---
+++ customfurniture/customfurniture3/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from customfurniture.users.models import User
+from customfurniture.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
